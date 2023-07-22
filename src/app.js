2d51b780223c84e40c7464c7fa13b1d440559a52
schaz98 <Shahzad98.ar@gmail.com>
2023-07-22 12:07:46 -0700

    added background

--- src/app.js
+++ src/app.js
@@ -34,13 +34,17 @@ function displayForecast(response) {
         `
       <div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+       
         <img
-          src="http://openweathermap.org/img/wn/${
-            forecastDay.weather[0].icon
-          }@2x.png"
-          alt=""
-          width="42"
-        />
+        src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
+        alt=""
+        width="42"
+      />
+
+
+        
         <div class="weather-forecast-temperatures">
             <span class="weather-forecast-temperature-max"> ${Math.round(
               forecastDay.temp.max
@@ -86,6 +90,8 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+
 
   getForecast(response.data.coord);
 }
